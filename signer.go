018f85feac911ca39ff3501316a648b725ec1a14
Petyr Darby <petyrdar@bo-a4.nov.21-school.ru>
2025-05-14 15:04:03 +0300

    all tests passed

--- signer.go
+++ signer.go
@@ -7,10 +7,7 @@ import (
 	"sync"
 )
 
-type MH struct {
-	data string
-	th   int
-}
+const HashWorkerCount = 6
 
 func ExecutePipeline(jobs ...job) {
 	if len(jobs) == 0 {
@@ -20,11 +17,11 @@ func ExecutePipeline(jobs ...job) {
 	var wg sync.WaitGroup
 	wg.Add(len(jobs))
 
-	in := make(chan interface{})
+	in := make(chan any)
 
 	for _, j := range jobs {
-		out := make(chan interface{})
-		go func(j job, in, out chan interface{}) {
+		out := make(chan any)
+		go func(j job, in, out chan any) {
 			j(in, out)
 			close(out)
 			wg.Done()
@@ -36,64 +33,92 @@ func ExecutePipeline(jobs ...job) {
 	wg.Wait()
 }
 
-func SingleHash(in chan interface{}, out chan interface{}) {
+func SingleHash(in chan any, out chan any) {
 	var wg sync.WaitGroup
-	th := 0
+	var md5Mutex sync.Mutex
 
-	for val := range in {
+	for data := range in {
 		wg.Add(1)
-		go func(val string, th int) {
-			var crc32Val string
-			var crc32Md5 string
-			var md5Val string
+		dataStr := strconv.Itoa(data.(int))
+
+		go func(inputData string) {
+			defer wg.Done()
+
+			var crc32Result, crc32Md5Result string
 
 			var innerWg sync.WaitGroup
 			innerWg.Add(2)
 
 			go func() {
 				defer innerWg.Done()
-				crc32Val = DataSignerCrc32(val)
+				crc32Result = DataSignerCrc32(inputData)
 			}()
 
 			go func() {
 				defer innerWg.Done()
-				md5Val = DataSignerMd5(val)
-				crc32Md5 = DataSignerCrc32(md5Val)
+
+				md5Mutex.Lock()
+				md5Val := DataSignerMd5(inputData)
+				md5Mutex.Unlock()
+
+				crc32Md5Result = DataSignerCrc32(md5Val)
 			}()
 
 			innerWg.Wait()
 
-			data := crc32Val + "~" + crc32Md5
-			out <- MH{data, th}
-			wg.Done()
-		}(strconv.Itoa(val.(int)), th)
-		th++
+			res := crc32Result + "~" + crc32Md5Result
+			out <- res
+		}(dataStr)
 	}
 
 	wg.Wait()
 }
 
-func MultiHash(in chan interface{}, out chan interface{}) {
+func MultiHash(in chan any, out chan any) {
 	var wg sync.WaitGroup
+	var mux sync.Mutex
 
-	for val := range in {
+	for data := range in {
 		wg.Add(1)
+		dataStr := data.(string)
 
-		go func(data MH) {
-			result := DataSignerCrc32(strconv.Itoa(data.th) + data.data)
+		go func(inputData string) {
+			defer wg.Done()
+
+			var innerWg sync.WaitGroup
+			hashes := make([]string, HashWorkerCount)
+
+			for th := 0; th < HashWorkerCount; th++ {
+				innerWg.Add(1)
+				go func(th int) {
+					hash := DataSignerCrc32(strconv.Itoa(th) + inputData)
+					mux.Lock()
+					defer mux.Unlock()
+					hashes[th] = hash
+					innerWg.Done()
+				}(th)
+			}
+
+			innerWg.Wait()
+
+			var result string
+			for th := 0; th < HashWorkerCount; th++ {
+				result += hashes[th]
+			}
+
 			out <- result
-			wg.Done()
-		}(val.(MH))
+		}(dataStr)
 	}
 
 	wg.Wait()
 }
 
-func CombineResults(in chan interface{}, out chan interface{}) {
+func CombineResults(in chan any, out chan any) {
 	result := make([]string, 0)
-	for val := range in {
-		data := val.(string)
-		result = append(result, data)
+
+	for data := range in {
+		dataStr := data.(string)
+		result = append(result, dataStr)
 	}
 
 	sort.Strings(result)
